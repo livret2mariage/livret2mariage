ae47748d5d37bddb27a25b3066cc968f4d1893a8
livret2mariage <vt.styles@gmail.com>
2026-08-31 10:21:48 +0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -83,6 +83,8 @@ const MIME = {
   ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
   ".json": "application/json; charset=utf-8",
   ".pdf": "application/pdf",
 };
